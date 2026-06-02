a0789b9eb7211d620180d38f07b1ff099c283eba
Kay0225123 <130005706+Kay0225123@users.noreply.github.com>
2026-06-02 17:10:17 -0300

    Update black_scholes.py

--- black_scholes.py
+++ black_scholes.py
@@ -1,14 +1,36 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from scipy.stats import norm
+import math
 
-def d1(S, K, T, r, sigma):
-    return (np.log(S / K) + (r + 0.5 * sigma**2) * T) / (sigma * np.sqrt(T))
+# Generating random array of values between 50 - 150
+#S is the stock price
+S = np.linspace(50, 150, 20)
+# strike price
+K = float(input("Enter the strike price: "))
+#Cumulative distribution function
+#N()
+#Time left til maturity (in years)
+T = float(input("Enter the time left till maturity(in years): "))
+#risk free rate
+r = float(input("Enter the trisk free rate: "))
+#Volatility
+sigma = float(input("Enter the volatility: "))
 
-def d2(S, K, T, r, sigma):
-    return d1(S, K, T, r, sigma) - sigma * np.sqrt(T)
+#Calculating D1 and D2 
+d1 = (np.log(S/K) + (r + (sigma**2)/2)*T) / (sigma*np.sqrt(T))
+d2 = d1 - sigma*np.sqrt(T)
 
-def call_price(S, K, T, r, sigma):
-    return S * norm.cdf(d1(S, K, T, r, sigma)) - K * np.exp(-r * T) * norm.cdf(d2(S, K, T, r, sigma))
+#Calculating Calls and Puts
+Call = S * norm.cdf(d1) - K * np.exp(-r*T) * norm.cdf(d2)
+Put = K * np.exp(-r*T) * norm.cdf(-d2) - S * norm.cdf(-d1)
 
-def put_price(S, K, T, r, sigma):
-    return K * np.exp(-r * T) * norm.cdf(-d2(S, K, T, r, sigma)) - S * norm.cdf(-d1(S, K, T, r, sigma))
+## I am gonna need two axes inside my figures
+fig, (ax1, ax2) = plt.subplots(ncols=2) 
+
+ax1.scatter(S, Call)
+ax2.scatter(S, Put)
+
+ax1.set_title('Call')
+ax2.set_title('Put')
+plt.show()
